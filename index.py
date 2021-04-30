a2c58ae16a6e5b5317109c42a4ba33daa427b311
CallMe AsYouFeel <futaiwata@gmail.com>
2021-04-30 23:11:24 +0900

    added discord server

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
 import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.environ['BOT_TOKEN']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('logged in')
+
+client.run(TOKEN)
